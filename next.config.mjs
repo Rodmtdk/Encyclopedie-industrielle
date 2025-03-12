e2f6ae727977fb37cbf8c85910d8150911b0c5f1
Rodmtdk <rod.mtdk.gng@gmail.com>
2025-03-12 10:45:11 +0000

    Correction finale : Génération du dossier out pour Next.js export

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,9 @@
 const nextConfig = {
   reactStrictMode: true,
   output: "export", // Indique à Next.js d'exporter en mode statique
+  distDir: "out", // Assure que Next.js exporte vers le bon dossier
   images: {
+    unoptimized: true, // Désactive l'optimisation des images (sinon, erreur avec "export")
     domains: ['source.unsplash.com', 'upload.wikimedia.org', 'www.youtube.com'],
     formats: ['image/avif', 'image/webp'],
   },
