427063a4db9eb07382a66e3e400ab4490daf0041
Rodmtdk <rod.mtdk.gng@gmail.com>
2025-03-12 10:40:26 +0000

    Correction: Ajout de output: 'export' pour générer un dossier out

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,9 @@
 const nextConfig = {
   reactStrictMode: true,
+  output: "export", // Indique à Next.js d'exporter en mode statique
   images: {
     domains: ['source.unsplash.com', 'upload.wikimedia.org', 'www.youtube.com'],
     formats: ['image/avif', 'image/webp'],
-  },
-  async rewrites() {
-    return [{ source: '/api/:path*', destination: 'https://external-api.com/:path*' }];
   },
 };
 
